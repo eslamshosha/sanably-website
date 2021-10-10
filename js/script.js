77dc5ae8f6f7d7da46b44babfe4a12cb7e4ee24b
eslam shosha <you@example.comeslamshosha22.es@gmail.com>
2021-10-10 15:33:37 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,73 @@
+$(document).ready(function() {
+	new WOW().init();
+
+	//phone size menu onclick
+	if ($(window).width() <= 1199) {
+        $('#menu-id').click(function (e) {
+           e.preventDefault()
+           $(".overlay-box").fadeIn(300);
+           $(".navgition").addClass("reset-left");
+           $("body").addClass("overflow");
+       });
+       $(".nav-head .close-btn, .overlay-box").click(function () {
+           $(".overlay-box").fadeOut(300);
+           $(".navgition").removeClass("reset-left");
+           $("body").removeClass("overflow");
+       });
+
+	   //dropdown inside menu
+	   $('.cat-li.dropdown .cat-anchor, .lang-word, .navgition .dropdown>a').click(function (e) {
+		e.preventDefault()
+		var item =  $(this).siblings(".dropdown-content");
+		item.slideToggle(500);
+		
+		})
+   	}
+
+       // //main Slider Carousel
+    ///////// ** main** /////////
+    var specials = new Swiper('.main-slider .swiper-container', {
+        loop: true,
+        autoplay: true,
+        pagination: {
+            el: '.main-slider .swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.main-slider .swiper-btn-next',
+            prevEl: '.main-slider .swiper-btn-prev',
+        },
+        breakpoints: {
+            0: {
+                slidesPerView: 1,
+            },
+            767: {
+                slidesPerView: 1,
+            },
+            992: {
+                slidesPerView: 1,
+            },
+            1199: {
+                slidesPerView: 1,
+            },
+        },
+    });
+
+});
+
+
+
+
+
+
+
+
+
+
+
+	
+	
+		
+	
+		
+
